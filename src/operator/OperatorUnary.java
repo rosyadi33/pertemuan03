ec22a44829c2bd56e1a53bf4d9004e3923ee90ff
ACER <mrosyadi23@outlook.com>
2020-04-12 16:19:44 +0800

    Operator Equlity dan Relational

--- src/operator/OperatorUnary.java
+++ src/operator/OperatorUnary.java
@@ -2,13 +2,15 @@ package operator;
 
 public class OperatorUnary {
     public static  void main(String[] args) {
-        int i = 0;
-        i++;
-        System.out.println(i);
-        i++;
-        System.out.println(i);
-        i=i+1;
-        System.out.println(i);
+        int i = 6;
+        int j = 7;
+        System.out.println(i==6);
+        System.out.println(i==7);
+        System.out.println(i==j);
+        System.out.println(i>j);
+        System.out.println(6>7);
+        System.out.println(i!=6);
+        System.out.println(j!=6);
     }
 
 }
