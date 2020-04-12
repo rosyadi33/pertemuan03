8e54f1d88c3bd9d6271b2f4446b6a72417e50172
ACER <mrosyadi23@outlook.com>
2020-04-12 16:30:02 +0800

    OperatorUnary

--- src/operator/OperatorUnary.java
+++ src/operator/OperatorUnary.java
@@ -2,15 +2,18 @@ package operator;
 
 public class OperatorUnary {
     public static  void main(String[] args) {
-        int i = 6;
-        int j = 7;
-        System.out.println(i==6);
-        System.out.println(i==7);
-        System.out.println(i==j);
-        System.out.println(i>j);
-        System.out.println(6>7);
-        System.out.println(i!=6);
-        System.out.println(j!=6);
+        int i = 0;
+        System.out.println(i);
+
+        i+=3;
+        System.out.println(i);
+
+        i*=3;
+        System.out.println(i);
+
+        i*=3;
+        System.out.println(i);
+
     }
 
 }
